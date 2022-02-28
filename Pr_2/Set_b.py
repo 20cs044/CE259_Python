59d40fc4aee98c1ae9558604232f9ee2db2034f0
20cs044 <91063808+20cs044@users.noreply.github.com>
2022-02-28 20:50:41 +0530

    Set_b.py

--- Pr_2/Set_b.py
+++ Pr_2/Set_b.py
@@ -1,1 +1,19 @@
+# Practical AIM             :-  b. Write a Python program to remove an item from a set if it is present in the set.
+# Student-ID                :-  20CS044
+# Student-Name              :-  DEVRAJ PARMAR
+# GitHub repository link    :-  https://github.com/20cs044/CE259_Python.git
 
+set1 = {"kishan", "prince", "preet", "jeet", "hit", "mit", "meet"}
+print(set1)
+name = input("insert a name to remove from a list")
+initial_length = len(set1)
+for i in set1:                  #for loop to check input item is present in the set or not?
+    if name == i:               #if statement to discard an item if ti is present in the set
+        set1.discard(name)
+        break
+final_length = len(set1)
+if initial_length == final_length:
+    print("item entered by you is not present inside the list")
+else :
+    print("item removed successfully from the set")
+print(set1)
