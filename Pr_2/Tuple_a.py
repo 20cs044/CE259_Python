84cae3031693ce7ea5fe4fd31b2b3e073abed80e
20cs044 <91063808+20cs044@users.noreply.github.com>
2022-02-28 20:57:18 +0530

    Tuple_a.py

--- Pr_2/Tuple_a.py
+++ Pr_2/Tuple_a.py
@@ -1,1 +1,7 @@
+# Practical AIM             :-  a. Write a Python program to create a tuple with different data types.
+# Student-ID                :-  20CS044
+# Student-Name              :-  DEVRAJ PARMAR
+# GitHub repository link    :-  https://github.com/20cs044/CE259_Python.git
 
+tuple1 = ("Devraj",2.131,False,982)     #tuple1 contains string,float,bool,and int
+print(tuple1)
