65bc26d6c042fc697a99e30930bbc3141259641a
20cs044 <91063808+20cs044@users.noreply.github.com>
2022-02-28 21:04:11 +0530

    Tuple_e.py

--- Pr_2/Tuple_e.py
+++ Pr_2/Tuple_e.py
@@ -1,1 +1,10 @@
+# Practical AIM             :-  e. Write a Python program to find the length of a tuple.
+# Student-ID                :-  20CS044
+# Student-Name              :-  DEVRAJ PARMAR
+# GitHub repository link    :-  https://github.com/20cs044/CE259_Python.git
 
+#create a tuple
+tuple8 =(12, 21, 32, 23, 43, 34, 54, 45)
+print(tuple8)
+#len() function to known the length of tuple
+print(len(tuple8))
