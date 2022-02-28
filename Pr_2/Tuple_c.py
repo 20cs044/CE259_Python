18c67e12b387d3ceb24f42dd34e876c888a7d7cb
20cs044 <91063808+20cs044@users.noreply.github.com>
2022-02-28 21:01:04 +0530

    Tuple_c.py

--- Pr_2/Tuple_c.py
+++ Pr_2/Tuple_c.py
@@ -1,1 +1,24 @@
+# Practical AIM             :-  c. Write a Python program to add an item in a tuple.
+# Student-ID                :-  20CS044
+# Student-Name              :-  DEVRAJ PARMAR
+# GitHub repository link    :-  https://github.com/20cs044/CE259_Python.git
 
+#create a tuple
+tuple3 = (4, 6, 2, 8, 3, 1)
+print(tuple3)
+
+#tuples are immutable
+#add an element using + operator and storing it in tuple4
+tuple4 = tuple3 + (9,)
+print(tuple4)
+
+#adding items in a specific index
+tuple5 = tuple3[:5] + (15, 20, 25)
+print(tuple5)
+
+#converting the tuple to list
+list1 = list(tuple3)
+#add an element in list using append
+list1.append(30)
+tuple6 = tuple(list1)   #converting list in tuple
+print(tuple6)
