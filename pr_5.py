92582bc01711c36537adf9151b58acc19254f2dc
20cs044 <91063808+20cs044@users.noreply.github.com>
2022-03-05 13:25:22 +0530

    pr_5.py

---
+++ pr_5.py
@@ -0,0 +1,9 @@
+# Practical AIM             :-  You are given a string and your task is to swap cases. 
+# Student-ID                :-  20CS044
+# Student-Name              :-  DEVRAJ PARMAR
+# GitHub repository link    :-  https://github.com/20cs044/CE259_Python.git
+
+
+Input_String = input()  #taking input as string
+#convert all lowercase letters to uppercase letters and vice versa. using .swapcase()
+print(Input_String.swapcase())
