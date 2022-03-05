ea325101270e10da7d4fd26d86ad3487cc8c91e6
20cs044 <91063808+20cs044@users.noreply.github.com>
2022-03-05 13:22:37 +0530

    pr_3.py

--- pr_3.py
+++ pr_3.py
@@ -1,6 +1,6 @@
 # Practical AIM             :-  Find Captain Room Number.
-# Student-ID                :-  20CS052
-# Student-Name              :-  KISHANKUMAR PATEL
+# Student-ID                :-  20CS044
+# Student-Name              :-  DEVRAJ PARMAR
 # GitHub repository link    :-  https://github.com/20cs044/CE259_Python.git
 
 
